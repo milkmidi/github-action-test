70e339c42bc6aff6ff3c309521f61dfd75d591eb
milkmidi <milkmidi.liao@positivegrid.com>
2025-06-16 14:53:46 +0800

    feat: add dotenv dependency and configure environment variables in index.js

---
+++ internals/index.js
@@ -0,0 +1,9 @@
+// env node js
+require('dotenv').config();
+
+const mockKey = process.env.MOCK_KEY;
+
+console.log('Mock Key:', mockKey);
+
+const secretKey = process.env.SECRET_KEY;
+console.log('Secret Key:', secretKey);
